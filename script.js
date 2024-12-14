d04b90f4c570afcc1624faa4ae70c90f1c3a75c3
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-14 20:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    const minHeap = new MinHeap();
+    let totalCost = 0;
+
+
+    for (const length of arr) {
+        minHeap.push(length);
+    }
+
+    while (minHeap.size() > 1) {
+        const first = minHeap.pop();
+        const second = minHeap.pop();
+        const cost = first + second;
+        totalCost += cost;
+        minHeap.push(cost);
+    }
+
+    return totalCost;
 }
+  
+console.log(mincost([4, 3, 2, 6])); 
+console.log(mincost([1, 2, 3, 4, 5])); 
 
 module.exports=mincost;
